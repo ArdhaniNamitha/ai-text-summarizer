1b185fede33456a2744c07580dc4f6e1cea1d136
student <student@example.com>
2025-07-10 09:59:48 +0530

    Fix: Set app to use environment PORT

--- app.py
+++ app.py
@@ -87,5 +87,6 @@ def download():
     return send_file(file_stream, as_attachment=True, download_name="summary.txt", mimetype="text/plain")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
